93fa59cfc2e294f3d0bc9e81c3f76c730e080fbd
michaia0363@naver.com <>
2023-03-17 18:01:27 +0900

    Fix: 루트로 배경움직이기 절반 성공

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -6,9 +6,9 @@ public class MainCamera : MonoBehaviour
 {
     public GameObject Target;               // 카메라가 따라다닐 타겟
 
-    public float offsetX = 0f;            // 카메라의 x좌표
+    public float offsetX = -3f;            // 카메라의 x좌표
     public float offsetY = 2f;           // 카메라의 y좌표
-    public float offsetZ = -7f;          // 카메라의 z좌표
+    public float offsetZ = -1f;          // 카메라의 z좌표
 
     public float CameraSpeed = 3f;       // 카메라의 속도
     Vector3 TargetPos;                      // 타겟의 위치
