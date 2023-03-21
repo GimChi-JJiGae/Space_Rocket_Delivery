a27695e35000fb109ceafdce4fcfddddae982c97
kimheeje5883@gmail.com <>
2023-03-21 16:07:46 +0900

    Feat: Animate Supplier

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class PlayerInteraction : MonoBehaviour
 {
     // 들 수 있는 모든 obj를 담을 liftableObjects를 리스트로 선언
-    public List<GameObject> LiftableObjects = new();
+    public List<string> HoldableObjects = new();
     // character의 왼손, 오른손, 상호작용 할 수 있는 물체와의 최대 거리 선언
     public Transform leftHand;
     public float maxDistance = 1f;
 
-    [SerializeField] private GameObject fuelPrefab;
-    [SerializeField] private GameObject orePrefab;
+    private GameObject fuelPrefab;
+    private GameObject orePrefab;
     //[SerializeField] private GameObject upgradePrefab;
     //[SerializeField] private GameObject laserPrefab;
 
@@ -30,26 +30,17 @@ public class PlayerInteraction : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         playerAnimator = GetComponent<Animator>();
 
-        Mesh fuelMesh = fuelPrefab.GetComponent<MeshFilter>().sharedMesh;
-        Mesh gasMesh = orePrefab.GetComponent<MeshFilter>().sharedMesh;
-        //Mesh upgradeMesh = upgradePrefab.GetComponent<MeshFilter>().sharedMesh;
-        //Mesh laserMesh = laserPrefab.GetComponent<MeshFilter>().sharedMesh;
+        GameObject dummyPrefab = GameObject.Find("Dummy");
 
-        foreach (GameObject obj in FindObjectsOfType<GameObject>())
+        foreach (Transform prefab in dummyPrefab.transform)
         {
-            MeshFilter objMeshFilter = obj.GetComponent<MeshFilter>();
-
-            if (objMeshFilter != null && (objMeshFilter.sharedMesh == fuelMesh ||
-                objMeshFilter.sharedMesh == gasMesh))
-            {
-                LiftableObjects.Add(obj);
-            }
+            HoldableObjects.Add(prefab.gameObject.name);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (currentObject == null && LiftableObjects.Contains(collision.gameObject))
+        if (HoldableObjects.Contains(collision.gameObject.name))
         {
             currentObject = collision.gameObject;
         }
@@ -57,7 +48,10 @@ public class PlayerInteraction : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        currentObject = null;
+        if (currentObject != null)
+        {
+            currentObject = null;
+        }
     }
 
     private void OnAnimatorIK(int layerIndex)
@@ -80,32 +74,16 @@ public class PlayerInteraction : MonoBehaviour
         {
             if (currentObject != null)
             {
-                if (!isHoldingObject)
+                if (isHoldingObject == false)
                 {
                     isHoldingObject = true;
 
-                    //currentObject.transform.parent = leftHand;
-                    //currentObject.transform.SetLocalPositionAndRotation(leftHand.position, leftHand.rotation);
-
-                    GameObject parentObject = new()
-                    {
-                        name = currentObject.name + "_parent"
-                    };
-                    parentObject.transform.SetPositionAndRotation(leftHand.position, leftHand.rotation);
-
-                    parentObject.transform.parent = leftHand;
-
-                    // Make the current object a child of the parent object
-                    currentObject.transform.parent = parentObject.transform;
-
-                    // Set the position and rotation of the current object relative to the parent object
+                    currentObject.transform.parent = leftHand;
                     currentObject.transform.SetLocalPositionAndRotation(leftHand.position, leftHand.rotation);
                 }
                 else
                 {
                     isHoldingObject = false;
-
-                    Destroy(currentObject.transform.parent.gameObject);
                     
                     currentObject.transform.parent = null;
 
