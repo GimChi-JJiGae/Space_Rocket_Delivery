8400cc752a6891dda67344c480b237ff42437d30
michaia0363@naver.com <>
2023-04-06 16:25:20 +0900

    checkin

--- Assets/Scripts/Spaceship/BasicTurretSpinController.cs
+++ Assets/Scripts/Spaceship/BasicTurretSpinController.cs
@@ -14,12 +14,28 @@ public class BasicTurretSpinController : MonoBehaviour
     {
         socketObj = GameObject.Find("SocketClient");
         controller = socketObj.GetComponent<Controller>();                   // 컨트롤러 연결하기
+
+
+        StartCoroutine(CallFunctionRepeatedly());
+    }
+
+
+    IEnumerator CallFunctionRepeatedly()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f); // 0.1초마다 반복
+                                                   // 반복해서 호출할 함수 호출
+            Quaternion currentSpin = transform.rotation;
+            Quaternion verticalSpin = TurretShootingHead.transform.rotation;
+            Debug.Log("포탑송신");
+            controller.Send(PacketType.MOVE, controller.roomCode, controller.userId, currentSpin.x, currentSpin.y, currentSpin.z, currentSpin.w, verticalSpin.x, verticalSpin.y, verticalSpin.z, verticalSpin.w);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("포탑송신");
         float horizontal = Input.GetAxis("Horizontal"); // 수평 입력 값
 
         if(socketObj == null)
@@ -37,8 +53,6 @@ public class BasicTurretSpinController : MonoBehaviour
             Vector3 rotationDirection = new Vector3(0, horizontal, 0f);
             transform.Rotate(rotationDirection * degree * Time.deltaTime);
         }
-        Quaternion currentSpin = transform.rotation;
-        Quaternion verticalSpin = TurretShootingHead.transform.rotation;
-        controller.Send(PacketType.MOVE, controller.roomCode, controller.userId, currentSpin.x, currentSpin.y, currentSpin.z, currentSpin.w, verticalSpin.x, verticalSpin.y, verticalSpin.z, verticalSpin.w);
+        
     }
 }
