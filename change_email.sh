90cbb5da9ef2d4ed619899a2201d34f9c0164a3b
michaia0363 <michaia0363@gmail.com>
2023-07-10 23:31:22 +0900

    커밋 이메일, 이름 변경 테스트

---
+++ change_email.sh
@@ -0,0 +1,16 @@
+git filter-branch --force --env-filter '
+OLD_EMAIL=michaia0363@naver.com"
+CORRECT_NAME="GimChi-JJiGae"
+CORRECT_EMAIL="michaia0363@gmail.com"
+
+if [ "$GIT_COMMITTER_NAME" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
